2d2208105b9ddb455359ede847a5dcb7469d3c96
qwertyuidfghjkw <162884795+qwertyuidfghjkw@users.noreply.github.com>
2024-05-26 21:33:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,14 @@ function sumFloats(...args) {
     return args.reduce((acc, val) => acc + (typeof val === 'number' ? val : 0), 0);
 }
 
+function sumArrays(...args) {
+    return args.reduce((acc, val) => {
+        if (Array.isArray(val)) {
+            return acc + val.reduce((subAcc, subVal) => subAcc + (typeof subVal === 'number' ? subVal : 0), 0);
+        }
+        return acc;
+    }, 0);
+}
+
 
 exports.sumIntegers = sumIntegers;
